95378137737906f260db4ed98105da59de221034
Youcef Harmouni <96540829+XareDev@users.noreply.github.com>
2022-09-17 01:48:14 +0100

    Add files via upload

--- Cadre_décoration/product_type_page.js
+++ Cadre_décoration/product_type_page.js
@@ -6,13 +6,3 @@ if( window.outerWidth <= 1080 &&  window.outerWidth >= 960) {
 	productSection.style.gridTemplateColumns ="repeat(1, 200px)";
 
 }
-
-
-if( window.outerWidth <= 600 &&  window.outerWidth >= 400) {
-	
-	productSection.style.gridTemplateRows = "repeat(2, 200px)";
-	productSection.style.gridTemplateColumns ="repeat(1, 200px)";
-	slideShow.style.height = "50vh";
-	
-}
-
